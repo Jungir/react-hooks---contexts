9760790887fb5d52d24516aed71fc73534327f61
Jungir <googlman6300@gmail.com>
2019-08-10 22:43:41 +0600

    Consume Multiple Contexts

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,34 @@
 import React, {Component} from 'react'
 import { ThemeContext } from '../contexts/ThemeContext';
+import { AuthContext } from '../contexts/AuthContext';
 
 class Navbar extends Component{
     
     
+    // to use with funcitons and classes, can consumer multiple context using this syntax
     render(){
         return(
-            // to use with funcitons and classes, can consumer multiple context using this syntax
-            <ThemeContext.Consumer>{(context)=>{
-                const {isLightTheme, light, dark} = context;
-                const theme = isLightTheme ? light : dark;
-                return(
-                    <nav className="Navbar" style={{background: theme.ui, color : theme.syntax}}>
-                    <h1>Context App</h1>
-                    <ul>
-                        <li>Home</li>
-                        <li>About</li>
-                        <li>Contact</li>
-                    </ul>
-                </nav>
-                )
-            }}</ThemeContext.Consumer>
-        )
+            <AuthContext.Consumer>{(authContext) => (
+                <ThemeContext.Consumer>{(themeContext) => {
+                    const {isAuth, toggleAuth} = authContext;
+                    const {isLightTheme, light, dark} = themeContext;
+                    const theme = isLightTheme ? light : dark;
+                    return(
+                        <nav className="Navbar" style={{background: theme.ui, color : theme.syntax}}>
+                        <h1>Context App</h1>
+                        <div onClick={toggleAuth}>
+                            {isAuth ? 'logged in ' : 'logged out'}
+                        </div>
+                        <ul>
+                            <li>Home</li>
+                            <li>About</li>
+                            <li>Contact</li>
+                        </ul>
+                    </nav>
+                    )
+                }}</ThemeContext.Consumer>
+            )}</AuthContext.Consumer>
+        );
     }
 }
 
