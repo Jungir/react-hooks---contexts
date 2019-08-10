1d7b8b65689f2085d9ce0fa4c5702f5713b76cee
Jungir <googlman6300@gmail.com>
2019-08-10 22:14:15 +0600

    add two types of consumer

---
+++ src/components/BookList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { ThemeContext } from '../contexts/ThemeContext';
+class BookList extends Component{
+    //can only be used with class component
+    static contextType = ThemeContext;
+    render(){
+        const {isLightTheme, light, dark} = this.context;
+        const theme = isLightTheme ? light : dark;
+        return (
+            <div className='BookList' style={{color: theme.syntax, background: theme.bg}}>
+                <ul>
+                    <li style={{background: theme.ui}}>The Way Of Kings</li>
+                    <li style={{background: theme.ui}}>The name of the Wind</li>
+                    <li style={{background: theme.ui}}>the final empire</li>
+                </ul>
+            </div>
+        )
+    }
+}
+export default BookList;
